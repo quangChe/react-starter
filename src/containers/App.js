74635ffb948543750e7ef6f71eabef579d9712c4
quangChe <quang.n.che@gmail.com>
2019-01-18 10:00:11 -0800

    Correct way to use setState to change states when new state relies on previous state.

--- src/containers/App.js
+++ src/containers/App.js
@@ -41,7 +41,8 @@ class App extends PureComponent {
       { id: 3, name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    toggleClicked: 0
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -72,7 +73,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
+    this.setState( (prevState, props) => { 
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }  
+    });
   }
 
   render () {
