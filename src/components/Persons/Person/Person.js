a084704b6bffdc1db94bc43a3d7060ec594ee742
quangChe <quang.n.che@gmail.com>
2019-01-18 10:55:11 -0800

    v16.3 way of using references by using React.createRef()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,7 @@ class Person extends Component {
   constructor(props) {
     super(props);
     console.log('Person.js -- constructor()');
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -15,7 +16,7 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('Person.js -- componentDidMount()');
-    if (this.props.position === 0) this.inputElement.focus();
+    if (this.props.position === 0) this.inputElement.current.focus();
   }
 
   componentWillUnmount() {
@@ -31,7 +32,7 @@ class Person extends Component {
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input 
-          ref={(inp) => this.inputElement = inp}
+          ref={this.inputElement}
           type="text" 
           onChange={this.props.changed} 
           value={this.props.name} 
